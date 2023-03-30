f37ec9d84cb4d462485374cea4ebcd5ce9682ff1
BrunoGuerreiro08 <bmguerreiro2012@gmail.com>
2023-03-29 22:26:00 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Video from './Pages/Video';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className = 'appVideos'>
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
+}
+
+export default App;
