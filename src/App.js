dddac738b7c10dc4cb06d55d268f5b5adc53b973
BrunoGuerreiro08 <bmguerreiro2012@gmail.com>
2023-04-03 14:38:25 -0300

    Só pra n perder nada

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import './App.css';
-import Video from './Pages/Video';
-
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore/lite";
+import "./App.css";
+import Video from "./Pages/Video";
+import db from "./config/Firebase";
 
 function App() {
+  
+  let maxHeight;
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight;
+  }
+  
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "Videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+    setVideos(videosList);
+  }
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
-    <div className="App">
-      <div className = 'appVideos'>
-        <Video 
-          likes = {1}
-          messages = {2}
-          shares = {3}
-          name = 'Nome'
-          description = 'Descrição'
-          music = 'Título da música - Autor'
-          url = "https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
-        />
-        <Video 
-          likes = {1}
-          messages = {2}
-          shares = {3}
-          name = 'Nome'
-          description = 'Descrição'
-          music = 'Título da música - Autor'
-          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4 "
-        />
+    <div className="App" style= {{maxHeight: maxHeight + "px"}}>
+      <div className="appVideos">
+        {video.map((item) => {
+          return(
+            <Video
+              likes = {item.likes}
+              messages = {item.messages}
+              shares = {item.shares}
+              name = {item.name}
+              music = {item.music}
+              description = {item.description}
+              url = {item.url}
+            />
+          )
+        })}
       </div>
     </div>
   );
